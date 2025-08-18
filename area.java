29e4df1b8810b4243750928081abea183f6e8dbb
artitripathi123 <artimini799@gmail.com>
2025-08-18 23:19:23 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class area {
+     public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      double var2 = var1.nextDouble();
+      double var4 = 3.14 * var2 * var2;
+      System.out.println(var4);
+    
+}
+}

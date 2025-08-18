9dac80c8e7256ad3ad5ecb7e4c18601b45efc09b
artitripathi123 <artimini799@gmail.com>
2025-08-18 23:21:05 +0530

    Add files via upload

---
+++ mile.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class mile {
+    public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      double Kilometers = var1.nextDouble();
+      double Miles = Kilometers * 0.621371;
+      System.out.println(Miles);
+     
+    
+}
+}

70f7084f218b681a5c3f4f174274bf279d49e1a6
artitripathi123 <artimini799@gmail.com>
2025-08-18 23:22:13 +0530

    Add files via upload

---
+++ volume.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class volume {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        double h=sc.nextDouble();
+        double volume=3.14*a*a*h;
+        System.out.println(volume);
+
+    
+}
+}

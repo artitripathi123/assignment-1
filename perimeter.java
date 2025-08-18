b9704e1b5693be82a284b79d4244aa3f3d675fb4
artitripathi123 <artimini799@gmail.com>
2025-08-18 23:21:55 +0530

    Add files via upload

---
+++ perimeter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class perimeter {
+    public static void main(String[] var0) {
+      Scanner var1 = new Scanner(System.in);
+      double var2 = var1.nextDouble();
+      double var3= var1.nextDouble();
+      double perimeter=2*(var2+var3);
+      System.out.println(perimeter);
+    
+}
+}
